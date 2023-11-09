2f393bf6f09839d6568dcd223443be3fde52157c
Shalemraja <82069264+Shalemraja777@users.noreply.github.com>
2023-11-09 13:22:33 +0530

    change

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Shalemraja777",
   resume:
-    "https://drive.google.com/file/d/1cSIM3G_cPqDy7w5Cz5ZdlTs_Nyn0BGJG/view?usp=share_link",
+    "https://drive.google.com/file/d/1XmV6QkkNOu47zx6SUIcgWs6Ra-08_bVi/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/shalem-raja-dandamudi/",
   twitter: "https://twitter.com/dandamudi_raja",
   insta: "https://www.instagram.com/_raja_chowdary_/",
